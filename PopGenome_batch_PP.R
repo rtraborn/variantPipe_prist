faffdd846de4dda274fae0a511dc07dab84209b2
rtraborn <rtraborn@indiana.edu>
2018-03-19 15:25:19 -0400

    Updated pipeline to incorporate annotation file.

--- PopGenome_batch_PP.R
+++ PopGenome_batch_PP.R
@@ -10,15 +10,20 @@ library(tools)
 vcfDir <- "/N/u/rtraborn/Carbonate/scratch/variantPipe/alignments/completed_vcfs/combined_vcf"
 vcfFile <- "P_pacificus_combined.vcf"
 myAnnot <- "/N/u/rtraborn/Carbonate/scratch/variantPipe/annotation/Hybrid2_AUGUSTUS2014_gene.gff3"
+popListFile <- "/N/dc2/scratch/rtraborn/variantPipe/pop_info/PP_popInfo.csv"
 ##########################################################################
 
 setwd(vcfDir)
 
-    #source("/N/dc2/projects/PromoterPopGen/DmPromoterPopGen/scripts/identifiers_to_list.R")
-    #pop.list <- identifiers_to_list(csv.file=popListFile)
+    source("/N/u/rtraborn/Carbonate/scratch/variantPipe/identifiers_to_list.R")
+    pop.list <- identifiers_to_list(csv.file=popListFile)
     GFF_split_into_scaffolds(myAnnot, "scaffoldGFFs")
-    VCF_split_into_scaffolds(vcfFile, "TaylorSplit")	  
+    VCF_split_into_scaffolds(vcfFile, "TaylorSplit")
     GENOME.class <- readData("TaylorSplit", format="VCF", gffpath="scaffoldGFFs")
-    GENOME.class
-    MKT(GENOME.class)
+    GENOME.class <- set.populations(GENOME.class, new.populations=pop.list, diploid=TRUE)  
+    GENOME.class.split <- splitting.data(GENOME.class, subsites="gene")
+    GENOME.class.split <- neutrality.stats(GENOME.class.split)
+    GENOME.class.split <- MKT(GENOME.class.split)
+    mkt_pp <- get.MKT(GENOME.class.split)
+    write.table(mkt_pp, file="mkt_pp_out.txt",col.names=TRUE)
 q()
