8e876a4776caf492175ab7508986a2a88069e187
rtraborn <rtraborn@indiana.edu>
2018-03-20 12:04:29 -0400

    Updated codebase to reflect syn/nonsyn analysis.

--- PopGenome_batch_PP.R
+++ PopGenome_batch_PP.R
@@ -9,7 +9,7 @@ library(tools)
 ##########################################################################
 vcfDir <- "/N/u/rtraborn/Carbonate/scratch/variantPipe/alignments/completed_vcfs/combined_vcf"
 vcfFile <- "P_pacificus_combined.vcf"
-myAnnot <- "/N/u/rtraborn/Carbonate/scratch/variantPipe/annotation/Hybrid2_AUGUSTUS2014_gene.gff3"
+myAnnot <- "/N/u/rtraborn/Carbonate/scratch/variantPipe/annotation/Hybrid2_AUGUSTUS2014_CDS_final.gff3"
 popListFile <- "/N/dc2/scratch/rtraborn/variantPipe/pop_info/PP_popInfo.csv"
 pristFaFile <- "/N/dc2/scratch/rtraborn/variantPipe/pacificus_Hybrid2.fasta"
 ##########################################################################
@@ -22,9 +22,10 @@ setwd(vcfDir)
     VCF_split_into_scaffolds(vcfFile, "TaylorSplit")
     GENOME.class <- readData("TaylorSplit", format="VCF", gffpath="scaffoldGFFs")
     GENOME.class <- set.populations(GENOME.class, new.populations=pop.list, diploid=TRUE)  
-    #GENOME.class <- set.synnonsyn(GENOME.class, ref.chr=pristFaFile, save.codons=TRUE)
+    GENOME.class <- set.synnonsyn(GENOME.class, ref.chr=pristFaFile, save.codons=TRUE)
+    GENOME.class@region.data@synonymous
     GENOME.class.split <- splitting.data(GENOME.class, subsites="gene")
-    GENOME.class.split <- neutrality.stats(GENOME.class.split)
+    #GENOME.class.split <- neutrality.stats(GENOME.class.split)
     GENOME.class.split <- MKT(GENOME.class.split)
     mkt_pp <- get.MKT(GENOME.class.split)
     write.table(mkt_pp, file="mkt_pp_out.txt",col.names=TRUE)
