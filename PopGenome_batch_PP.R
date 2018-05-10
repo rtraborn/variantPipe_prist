3b1cfa0d094150951fdb837119edfe1f1044a4b6
rtraborn <rtraborn@indiana.edu>
2018-05-10 14:04:21 -0400

    Added updated scripts after new variant calling pipeline.

--- PopGenome_batch_PP.R
+++ PopGenome_batch_PP.R
@@ -7,13 +7,12 @@ library(bigmemory)
 library(tools)
 
 ##########################################################################
-vcfDir <- "/N/u/rtraborn/Carbonate/scratch/variantPipe/alignments/completed_vcfs/combined_vcf"
-vcfFile <- "P_pacificus_combined.vcf"
-myAnnot <- "/N/u/rtraborn/Carbonate/scratch/variantPipe/annotation/Hybrid2_AUGUSTUS2014_CDS_final.gff3"
+vcfDir <- "/N/u/rtraborn/Carbonate/scratch/variantPipe/el_paco_vcf"
+vcfFile <- "P_pacificus_combined.vcf.gz"
+myAnnot <- "/N/u/rtraborn/Carbonate/scratch/variantPipe/annotation/El_Paco_gene_annotations_genes.gff"
+annotDir <- "/N/u/rtraborn/Carbonate/scratch/variantPipe/gff3_file"
 popListFile <- "/N/dc2/scratch/rtraborn/variantPipe/pop_info/PP_popInfo.csv"
-pristFaFile <- "/N/dc2/scratch/rtraborn/variantPipe/fasta/pacificus_Hybrid2.fasta"
-#pristFaFile <- "/N/dc2/scratch/rtraborn/variantPipe/pacificus_Hybrid2_first10.fa"
-pristFasta <- "/N/u/rtraborn/Carbonate/scratch/variantPipe/fasta/PP_scaffolds_test"
+pristFaFile <- "/N/dc2/projects/Pristionchus/MKT_shared/El_Paco_genome.fa"
 ##########################################################################
 
 setwd(vcfDir)
@@ -22,13 +21,14 @@ setwd(vcfDir)
     pop.list <- identifiers_to_list(csv.file=popListFile)
     GFF_split_into_scaffolds(myAnnot, "scaffoldGFFs")
     VCF_split_into_scaffolds(vcfFile, "TaylorSplit")
-    #GENOME.class <- readData("TaylorSplit", format="VCF", gffpath="scaffoldGFFs")
     GENOME.class <- readData("TaylorSplit", format="VCF", gffpath="scaffoldGFFs")
+    #GENOME.class <- readData(vcfDir, format="VCF", gffpath=annotDir)
     GENOME.class <- set.populations(GENOME.class, new.populations=pop.list, diploid=TRUE)  
     GENOME.class <- neutrality.stats(GENOME.class)
     GENOME.class <- set.synnonsyn(GENOME.class, save.codons=TRUE, ref.chr=rep(pristFaFile, length(GENOME.class@region.names)))
+    #GENOME.class <- set.synnonsyn(GENOME.class, save.codons=TRUE, ref.chr=c(scaff1.fas,scaff2.fas,...))
     GENOME.class@region.data@synonymous
-    GENOME.class.split <- splitting.data(GENOME.class, subsites="gene")
+    GENOME.class.split <- splitting.data(GENOME.class, subsites="gene", whole.data=FALSE)
     GENOME.class.split <- neutrality.stats(GENOME.class.split)
     #GENOME.class.split@region.data@synonymous
     GENOME.class.split <- set.synnonsyn(GENOME.class.split, save.codons=TRUE, ref.chr=pristFaFile)
---
+++ PopGenome_batch_updated.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#PBS -N PopGenome_batch_prist
+#PBS -k o
+#PBS -l nodes=1:ppn=16,vmem=80gb
+#PBS -l walltime=10:00:00
+
+module load r
+
+WD=/N/u/rtraborn/Carbonate/scratch/variantPipe
+
+cd $WD
+
+echo "Starting job"
+
+R CMD BATCH PopGenome_batch_PP.R
+
+echo "Job complete"
+
+exit
