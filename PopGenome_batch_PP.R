674c360a965ba235b78325aa381bb311bfd463fb
rtraborn <rtraborn@indiana.edu>
2018-05-16 14:59:51 -0400

    Updated pipeline; now the error is with setting synon info, not import.

--- PopGenome_batch_PP.R
+++ PopGenome_batch_PP.R
@@ -8,10 +8,11 @@ library(tools)
 
 ##########################################################################
 vcfDir <- "/N/u/rtraborn/Carbonate/scratch/variantPipe/el_paco_vcf"
-vcfFile <- "P_pacificus_combined.vcf.gz"
+vcfFile <- "P_pacificus_combined.vcf"
 myAnnot <- "/N/u/rtraborn/Carbonate/scratch/variantPipe/annotation/El_Paco_gene_annotations_v1_gene.gff"
-popListFile <- "/N/dc2/scratch/rtraborn/variantPipe/pop_info/PP_popInfo.csv"
+popListFile <- "/N/dc2/scratch/rtraborn/variantPipe/pop_info/PP_popInfo_updated.csv"
 pristFaFile <- "/N/dc2/projects/Pristionchus/MKT_shared/El_Paco_genome.fa"
+pristChr <- c("ChrI.fa","ChrII.fa","ChrIII.fa","ChrIV.fa","ChrV.fa","ChrX.fa", "pbcontig117.fa","pbcontig118.fa","pbcontig2839.fa","pbcontig2855.fa","pbcontig2856.fa","pbcontig2858.fa","pbcontig2865.fa","pbcontig2867.fa","pbcontig2876.fa","pbcontig2879.fa","pbcontig2880.fa","pbcontig2881.fa","pbcontig2882.fa","pbcontig2884.fa","pbcontig2886.fa","pbcontig2887.fa","pbcontig2890.fa","pbcontig2894.fa","pbcontig2897.fa","pbcontig2898.fa","pbcontig2.fa","pbcontig323.fa","pbcontig410.fa","pbcontig508.fa","pbcontig514.fa","pbcontig517.fa","pbcontig519.fa","pbcontig544.fa","pbcontig585.fa","pbcontig627.fa","pbcontig654.fa","pbcontig660.fa","pbcontig695.fa","pbcontig709.fa","pbcontig710.fa","pbcontig72.fa","pbcontig741.fa","pbcontig744.fa","pbcontig745.fa","pbcontig759.fa","pbcontig787.fa")
 ##########################################################################
 
 setwd(vcfDir)
@@ -24,8 +25,7 @@ setwd(vcfDir)
     #GENOME.class <- readData(vcfDir, format="VCF", gffpath=annotDir)
     GENOME.class <- set.populations(GENOME.class, new.populations=pop.list, diploid=TRUE)  
     GENOME.class <- neutrality.stats(GENOME.class)
-    GENOME.class <- set.synnonsyn(GENOME.class, save.codons=TRUE, ref.chr=rep(pristFaFile, length(GENOME.class@region.names)))
-    #GENOME.class <- set.synnonsyn(GENOME.class, save.codons=TRUE, ref.chr=c(scaff1.fas,scaff2.fas,...))
+    GENOME.class <- set.synnonsyn(GENOME.class, save.codons=TRUE, ref.chr=pristChr)
     GENOME.class@region.data@synonymous
     GENOME.class.split <- splitting.data(GENOME.class, subsites="gene", whole.data=FALSE)
     GENOME.class.split <- neutrality.stats(GENOME.class.split)
